d229a4dcb38da25bb97fd14bca0b241cf8dc8dbc
yulduz1007 <soipovaa7@gmail.com>
2025-02-20 17:56:28 +0500

    hcbshjvb

--- root/settings.py
+++ root/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3m3n_*&q$hlzn(v5@u1ef$uq@(x9f7ayg83urz8h_5^0)=lco$'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
